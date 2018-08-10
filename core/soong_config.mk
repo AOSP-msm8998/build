4cd4588e5e43a055ff0ba26ad622ffc7bc987e6b
Simon Shields <simon@lineageos.org>
2018-08-10 14:39:32 +1000

    soong: include custom soong_config
    Change-Id: Ic7b8993f4a201f8a9d24c6ba32617fa34843afc2

--- core/soong_config.mk
+++ core/soong_config.mk
@@ -399,6 +399,8 @@ $(call add_json_bool, NextReleaseHideFlaggedApi, $(filter true,$(PRODUCT_NEXT_RE
 
 $(call add_json_bool, BuildFromSourceStub, $(findstring true,$(PRODUCT_BUILD_FROM_SOURCE_STUB) $(BUILD_FROM_SOURCE_STUB)))
 
+include vendor/aosp/build/soong/soong_config.mk
+
 $(call json_end)
 
 $(file >$(SOONG_VARIABLES).tmp,$(json_contents))
